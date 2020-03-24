d900bb520f8398d40f8d367ad49fe7215060680f
Vb Streetz <vbstreetz@gmail.com>
2020-03-24 22:51:20 +0300

    improvement: rename

--- frontend/bin/electron.js
+++ frontend/bin/electron.js
@@ -10,14 +10,18 @@ const { PORT } = process.env;
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
-  mainWindow.loadURL(isDev ? `http://localhost:${PORT}` : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow.loadURL(
+    isDev
+      ? `http://localhost:${PORT}`
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
   if (isDev) {
     // Open the DevTools.
     //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
     mainWindow.webContents.openDevTools();
   }
-  mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.on('closed', () => (mainWindow = null));
 }
 
 app.on('ready', createWindow);
